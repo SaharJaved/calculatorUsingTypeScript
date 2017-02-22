b17d45c00f0a6f426e333808f7c70033b47420c8
Sahar <saharrjaved@gmail.com>
2017-02-22 08:14:59 -0800

    Modifications

--- javascript/typscript.js
+++ javascript/typscript.js
@@ -1,122 +1,100 @@
-/**
- * Created by wahaj on 2/10/2017.
- */
-var message = "Hello World";
-console.log(message);
-var calculations = (function () {
-    function calculations() {
+var calculation = (function () {
+    function calculation() {
+        this.number_1 = "";
+        this.number_2 = "";
+        this.flag = true;
+        this.result = "";
+        this.operators = function (value) {
+            if (value == '+') {
+                this.operator = '+';
+                console.log(this.operator);
+                this.flag = false;
+                document.getElementById("text").value = this.operator;
+            }
+            else if (value == '-') {
+                this.operator = '-';
+                console.log(this.operator);
+                document.getElementById("text").value = this.operator;
+                this.flag = false;
+            }
+            else if (value == '/') {
+                this.operator = '/';
+                console.log(this.operator);
+                document.getElementById("text").value = this.operator;
+                this.flag = false;
+            }
+            else if (value == '*') {
+                this.operator = '*';
+                console.log(this.operator);
+                document.getElementById("text").value = this.operator;
+                this.flag = false;
+            }
+        };
+        this.digit = function (value) {
+            console.log(this.flag);
+            if (this.flag) {
+                console.log(this.number_1);
+                this.number_1 += value;
+                document.getElementById("text").value = this.number_1;
+                console.log(this.number_1);
+                console.log(this.flag);
+            }
+            else {
+                parseFloat(this.number_2);
+                this.number_2 += value;
+                document.getElementById("text").value = this.number_2;
+                console.log(this.flag);
+                console.log(this.number_2);
+                console.log(this.flag);
+            }
+        };
+        this.calculation = function () {
+            switch (this.operator) {
+                case '+': {
+                    this.result = parseFloat(this.number_1) + parseFloat(this.number_2);
+                    parseFloat(this.result);
+                    document.getElementById("text").value = this.result;
+                    document.getElementById("showValue").value = "add";
+                    break;
+                }
+                case '-': {
+                    this.result = parseFloat(this.number_1) * parseFloat(this.number_2);
+                    parseFloat(this.result);
+                    document.getElementById("text").value = this.result;
+                    document.getElementById("showValue").value = "subtract";
+                    break;
+                }
+                case '/': {
+                    this.result = parseFloat(this.number_1) / parseFloat(this.number_2);
+                    parseFloat(this.result);
+                    document.getElementById("text").value = this.result;
+                    document.getElementById("showValue").value = "divide";
+                    break;
+                }
+                case '*': {
+                    this.result = parseFloat(this.number_1) * parseFloat(this.number_2);
+                    parseFloat(this.result);
+                    document.getElementById("text").value = this.result;
+                    document.getElementById("showValue").value = "multiply";
+                    break;
+                }
+                default: {
+                    document.getElementById("text").value = "error";
+                    break;
+                }
+            }
+            this.number_1 = this.result;
+            this.number_2 = "";
+        };
     }
-    calculations.prototype.addition = function (n1, n2) {
-        return n1 + n2;
+    calculation.prototype.clearRes = function () {
+        this.number_1 = '';
+        this.number_2 = '';
+        this.result = '';
+        document.getElementById("text").value = '';
+        console.log(this.number_1);
+        this.flag = true;
     };
-    calculations.prototype.subtraction = function (n1, n2) {
-        return n1 - n2;
-    };
-    calculations.prototype.multiplication = function (n1, n2) {
-        return n1 * n2;
-    };
-    calculations.prototype.division = function (n1, n2) {
-        return n1 / n2;
-    };
-    calculations.prototype.operator = function (op) {
-        switch (op) {
-            case '+': {
-                op = '+';
-                console.log(op);
-                break;
-            }
-            case '-': {
-                op = '-';
-                console.log(op);
-                break;
-            }
-            case '*': {
-                op = '*';
-                console.log(op);
-                break;
-            }
-            case '/': {
-                op = '/';
-                console.log(op);
-                break;
-            }
-            default: {
-                console.log("Invalid choice");
-                break;
-            }
-        }
-        return op;
-    };
-    return calculations;
+    return calculation;
 }());
-var op = new calculations();
-var operator;
-var number_1 = "";
-var number_2 = "";
-var flag = true;
-var result;
-var operators = function (value) {
-    operator = op.operator(value);
-    flag = false;
-};
-function digit(value) {
-    console.log(flag);
-    if (flag) {
-        console.log(number_1);
-        number_1 += value;
-        document.getElementById("text").value = number_1;
-        console.log(number_1);
-        console.log(flag);
-    }
-    else {
-        parseFloat(number_2);
-        number_2 += value;
-        document.getElementById("text").value = number_2;
-        console.log(flag);
-        console.log(number_2);
-        console.log(flag);
-    }
-}
-function res() {
-    switch (operator) {
-        case '+': {
-            result = op.addition(parseFloat(number_1), parseFloat(number_2));
-            parseFloat(result);
-            document.getElementById("text").value = result;
-            break;
-        }
-        case '-': {
-            result = op.subtraction(parseFloat(number_1), parseFloat(number_2));
-            parseFloat(result);
-            document.getElementById("text").value = result;
-            break;
-        }
-        case '*': {
-            result = op.multiplication(parseFloat(number_1), parseFloat(number_2));
-            parseFloat(result);
-            document.getElementById("text").value = result;
-            break;
-        }
-        case '/': {
-            result = op.division(parseFloat(number_1), parseFloat(number_2));
-            parseFloat(result);
-            document.getElementById("text").value = result;
-            break;
-        }
-        default: {
-            console.log("Invalid choice");
-            break;
-        }
-    }
-    number_1 = result;
-    console.log(number_1);
-    number_2 = "";
-}
-function clearRes() {
-    number_1 = '';
-    number_2 = '';
-    result = '';
-    document.getElementById("text").value = '';
-    console.log(number_1);
-    flag = true;
-}
+var cl = new calculation();
--- javascript/typscript.ts
+++ javascript/typscript.ts
@@ -1,157 +1,125 @@
-/**
- * Created by wahaj on 2/10/2017.
- */
-var message: string = "Hello World"
-console.log(message)
+class calculation {
+    operator: string;
+    number_1: string = "";
+    number_2: string = "";
+    flag: boolean = true;
+    result: string = "";
 
-class calculations
-{
-
-
-
-    addition(n1:number,n2:number):number{
-        return n1+n2;
-    }
-    subtraction(n1:number,n2:number):number{
-        return n1-n2;
+    clearRes() {
+        this.number_1 = '';
+        this.number_2 = '';
+        this.result = '';
+        (document.getElementById("text")as HTMLInputElement).value = '';
+        console.log(this.number_1);
+        this.flag = true;
     }
 
-    multiplication(n1:number,n2:number):number{
-        return n1*n2;
+
+    operators = function (value) {
+        if (value == '+') {
+            this.operator = '+';
+            console.log(this.operator);
+            this.flag = false;
+            (document.getElementById("text")as HTMLInputElement).value = this.operator;
+
+        }
+        else if (value == '-') {
+            this.operator = '-';
+            console.log(this.operator);
+            (document.getElementById("text") as HTMLInputElement).value = this.operator;
+            this.flag = false;
+        }
+        else if (value == '/') {
+            this.operator = '/';
+            console.log(this.operator);
+            (document.getElementById("text")as HTMLInputElement).value = this.operator;
+            this.flag = false;
+        }
+        else if (value == '*') {
+            this.operator = '*';
+            console.log(this.operator);
+            (document.getElementById("text") as HTMLInputElement).value = this.operator;
+            this.flag = false;
+        }
     }
 
-    division(n1:number,n2:number):number{
-        return n1/n2;
+    digit = function (value) {
+
+        console.log(this.flag);
+
+
+        if (this.flag) {
+
+            console.log(this.number_1);
+
+            this.number_1 += value;
+
+
+            (document.getElementById("text")as HTMLInputElement).value = this.number_1;
+            console.log(this.number_1);
+            console.log(this.flag);
+
+        }
+        else {
+            parseFloat(this.number_2);
+
+            this.number_2 += value;
+            (document.getElementById("text") as HTMLInputElement).value = this.number_2;
+            console.log(this.flag);
+
+            console.log(this.number_2);
+            console.log(this.flag);
+
+        }
     }
 
-    operator(op:string):string
-    {
-        switch(op) {
+    calculation = function () {
+
+
+        switch (this.operator) {
             case '+': {
-                op='+';
-                console.log(op);
 
+                this.result = parseFloat(this.number_1) + parseFloat(this.number_2);
+                parseFloat(this.result);
+                (document.getElementById("text")as HTMLInputElement).value = this.result;
+                (document.getElementById("showValue") as HTMLInputElement).value = "add"
+                break;
+
+
+            }
+
+
+            case '-': {
+                this.result = parseFloat(this.number_1) * parseFloat(this.number_2);
+                parseFloat(this.result);
+                (document.getElementById("text")as HTMLInputElement).value = this.result;
+                (document.getElementById("showValue") as HTMLInputElement).value = "subtract"
                 break;
             }
-            case '-': {
-                op='-';
-                console.log(op);
-
+            case '/': {     
+                this.result = parseFloat(this.number_1) / parseFloat(this.number_2);
+                parseFloat(this.result);
+                (document.getElementById("text")as HTMLInputElement).value = this.result;
+                (document.getElementById("showValue") as HTMLInputElement).value = "divide"
                 break;
             }
             case '*': {
-                op='*';
-                console.log(op);
-
-                break;
-            }
-            case '/': {
-                op='/';
-                console.log(op);
-
+                this.result = parseFloat(this.number_1) * parseFloat(this.number_2);
+                parseFloat(this.result);
+                (document.getElementById("text")as HTMLInputElement).value = this.result;
+                (document.getElementById("showValue") as HTMLInputElement).value = "multiply"
                 break;
             }
             default: {
-                console.log("Invalid choice");
+                (document.getElementById("text")as HTMLInputElement).value = "error";
+
                 break;
             }
         }
-        return op;
-    }
-
-
-
-
-
-
-}
-
-let op=new calculations();
-var operator;
-var number_1 = "";
-var number_2 = "";
-var flag = true;
-var result ;
-
-var operators = function (value) {
- operator= op.operator(value);
-flag=false;
-  }
-
-function digit(value) {
-
-    console.log(flag);
-
-
-
-    if (flag) {
-
-        console.log(number_1);
-
-        number_1 += value;
-
-
-        (document.getElementById("text") as HTMLInputElement).value = number_1;
-        console.log(number_1);
-        console.log(flag);
+        this.number_1 = this.result;
+        this.number_2 = "";
 
     }
-    else {
-        parseFloat(number_2);
-
-        number_2 += value;
-        (document.getElementById("text") as HTMLInputElement).value = number_2;
-        console.log(flag);
-
-        console.log(number_2);
-        console.log(flag);
-
-    }
-}
-function res() {
-    switch (operator) {
-        case '+': {
-            result =op.addition(parseFloat(number_1),parseFloat(number_2));
-            parseFloat(result);
-            (document.getElementById("text") as HTMLInputElement).value = result;
-
-            break;
-        }
-        case '-': {
-            result =op.subtraction(parseFloat(number_1),parseFloat(number_2));
-            parseFloat(result);
-            (document.getElementById("text") as HTMLInputElement).value = result;
-            break;
-        }
-        case '*': {
-            result =op.multiplication(parseFloat(number_1),parseFloat(number_2));
-            parseFloat(result);
-            (document.getElementById("text") as HTMLInputElement).value = result;
-            break;
-        }
-        case '/': {
-            result =op.division(parseFloat(number_1),parseFloat(number_2));
-            parseFloat(result);
-            (document.getElementById("text") as HTMLInputElement).value = result;
-            break;
-        }
-        default: {
-            console.log("Invalid choice");
-            break;
-        }
-    }
-    number_1 = result;
-    console.log(number_1);
-    number_2 = "" ;
-
 
 }
-
-function clearRes() {
-    number_1 = '';
-    number_2 = '';
-    result = '';
-    (document.getElementById("text") as HTMLInputElement).value = '';
-    console.log(number_1);
-    flag = true;
-}
+let cl = new calculation();
